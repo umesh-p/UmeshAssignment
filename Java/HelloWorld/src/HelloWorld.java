b242be1e02070abbac640b7594202160e150dcbe
dbda <dbda>
2017-02-22 19:35:04 +0530

    Hello World code added in the repo

---
+++ Java/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld
+{
+	public static void main(String[] args){
+		//System.out.println("Hello World");
+		int a,b,c;
+		System.out.println("Enter Number");
+		Scanner a1 = new Scanner(System.in);
+		a = a1.nextInt();
+		b = a1.nextInt();
+		c = a+b;
+		System.out.println("Number "+c);	
+		
+	}
+	
+}
+ 
